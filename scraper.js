3b20e8470ac4d96b238596c700c90618a6f3a579
Soltan <iamkazakh02@gmail.com>
2024-07-24 14:51:09 +0500

    fixing puppeteer deploy

--- scraper.js
+++ scraper.js
@@ -1,7 +1,9 @@
 const puppeteer = require('puppeteer');
 
 const scrapeKaspi = async () => {
-  const browser = await puppeteer.launch({ headless: false }); // Set to true for headless mode
+  const browser = await puppeteer.launch({ headless: false,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']  
+   }); // Set to true for headless mode
   const page = await browser.newPage();
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
   
